4b7e13b0080d72940afe23a7ac81781469f0f6db
Roman <semigranrv@gmail.com>
2016-09-23 15:44:40 +0300

    add directive

---
+++ directives/directive.js
@@ -0,0 +1,16 @@
+angular.module('battleApp').
+    directive('changeClass', function(){
+    return {
+        restrict: 'A',
+        link: function (scope, element) {
+            scope.$watch('shipdead',function() {
+                if(scope.shipdead) {
+                    for(var i=0; i<scope.shipdead.locations.length;i++) {
+                        var loc = document.getElementById(scope.shipdead.locations[i])
+                        loc.classList.add('killed')
+                    }
+                }
+            },true)
+        }
+    }
+    });
